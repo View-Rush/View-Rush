0b7fc51c9c87325a63a7152bc6719105aac6e0f7
De-Silva-Madhushankha <madhushankhades1@gmail.com>
2025-09-19 18:19:33 +0530

    refactor: remove unused TypeScript configuration files and update tsconfig.json structure

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig(({}) => ({
   server: {
     host: "::", // Listen on all IPv4 and IPv6 addresses
     port: 8080,
