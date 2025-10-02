74fc5e89b1f61a8fe02e8cecb2799d9e210897af
Bawantha Madhushankha De Silva <113349359+De-Silva-Madhushankha@users.noreply.github.com>
2025-10-02 20:36:00 +0530

    Update config.py

--- server/config.py
+++ server/config.py
@@ -7,4 +7,4 @@ env_path = Path(__file__).parent / ".env"
 env_vars = dotenv_values(dotenv_path=env_path)
 
 YOUTUBE_API_KEY = env_vars.get("YOUTUBE_API_KEY")
-print("Loaded API Key:", YOUTUBE_API_KEY) 
+print("Loaded API Key is {}".format("set" if YOUTUBE_API_KEY else "not set"))
